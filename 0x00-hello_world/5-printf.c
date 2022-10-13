e940d3bc640358db9cdef8940f4fa0b4eb4f3e90
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-13 15:18:55 +0000

    created 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program to use printf to display messages
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
