1ea5c0f99d349b8db6172064c9f45dc844e4da6a
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-13 14:09:01 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry into program
+ * return - set to a positive integer or zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multi
+lingual puzzle");
+	return (0);
+}
