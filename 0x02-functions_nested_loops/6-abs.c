45a51e04da1d98b3072129c095a6e0cd40fda3dc
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-18 20:44:26 +0000

    6-abs.c file created

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @h: find absolute value of a number
+ *
+ * Return: absolute value
+ */
+int _abs(int h)
+{
+	if (h < 0)
+	{
+		return (-h);
+	}
+	return (h);
+}
+
