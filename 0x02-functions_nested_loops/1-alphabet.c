eeab262a0793a0d7197a62557e108601513f1660
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-18 17:13:36 +0000

    1-alphabet.c file created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - write the lowercase alphabets
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
  * print_alpha - This will print lower case alphabet.
  */
 int _putchar(char k);
+void print_alphabet(void);
 
