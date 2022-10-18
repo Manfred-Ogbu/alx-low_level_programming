b28ad41ff0d453a7ca3f39588d41a889af5737c6
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-18 19:12:26 +0000

    3-islower.c file created

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @k: test for character
+ * Return: returns 1 for True and 0 for false
+ */
+int _islower(int k)
+{
+	return (k >= 'a' && k <= 'z');
+}
+
