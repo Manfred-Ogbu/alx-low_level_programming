d3aaffccbbe1bfd9b50128f53f38d1725df8ed05
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-20 19:31:27 +0000

    0 Task created.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - This will test the function for positve or negative
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
