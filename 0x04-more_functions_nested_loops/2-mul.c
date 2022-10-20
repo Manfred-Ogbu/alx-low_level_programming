14c4b2ad030c32e6b50f1297ac34e9a98f19871b
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-20 22:46:29 +0000

    2-mul.c created

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 values
+ * @m: first value to be supplied.
+ * @n: second value to be supplied.
+ *
+ * Return: The product
+ */
+int mul(int m, int n)
+{
+		return (m * n);
+}
+
