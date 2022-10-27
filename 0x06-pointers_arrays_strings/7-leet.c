2acf45ebace9d3e2a074d7217969a5723b5c7d1f
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-27 22:02:03 +0000

    7-leet.c updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,35 +1,26 @@
 #include "main.h"
 
 /**
- * leet - breaking out the leet speak.
- * @s: string to make 1337
+ * leet - encodes a string to 1337.
+ * @str: string to make 1337
  *
- * Return: a point the s
+ * Return: a pointer to the encoded string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	char lt[10][2] = {
-		{'a', '0' + 4}, {'A', '0' + 4},
-		{'e', '0' + 3}, {'E', '0' + 3},
-		{'o', '0'}, {'O', '0'},
-		{'t', '0' + 7}, {'T', '0' + 7},
-		{'l', '0' + 1}, {'L', '0' + 1}};
-	int y = 0;
-	int u = 0;
+	int p = 0;
+	int v;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (s[y] != '\0')
+	while (str[p])
 	{
-		while (lt[u][0] != '\0')
+		for (v = 0 ; v <= 7 ; v++)
 		{
-			if (s[y] == lt[u][0])
-			{
-				s[y] = lt[u][1];
-				u++;
-			}
+			if (str[p] == leet[v] || str[p] - 32 == leet[v])
+				str[p] = v + '0';
 		}
-		u = 0;
-		y++;
+		p++;
 	}
-	return (s);
+	return (str);
 }
 
