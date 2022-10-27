4fd3ee773837648e1b081fee64e172b16fe15f09
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-27 21:26:57 +0000

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,18 +8,16 @@
  */
 char *string_toupper(char *s)
 {
-	int p;
+	int p = 0;
 
-	p = 0;
-	while (s[p] != '\0')
+	while (s[p])
 	{
 		if (s[p] >= 'a' && s[p] <= 'z')
 		{
 			s[p] -= 32;
-			p++;
 		}
+		p++;
 	}
-	s[p] = '\0';
 
 	return (s);
 }
