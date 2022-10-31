d605452f54ed87910e994d78ddb907501a6de1d1
Manfred Ogbu <bizzyman007@gmail.com>
2022-10-31 18:34:02 +0000

    file created

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - fxn that sets the value of a pointer to a char.
+ * @s: source
+ * @to: string to copy to.
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+		*s = to;
+}
+
