11c81920b18e81e2311d649136204e629a449558
Manfred Ogbu <bizzyman007@gmail.com>
2022-11-02 13:48:45 +0000

    1-print_rev_recursion.c file created

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - fxn that prints a string in reverse.
+ * @s: string to print in reverse.
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+	{
+		return;
+	}
+	_print_rev_recursion((s + 1));
+	_putchar(*s);
+}
+
